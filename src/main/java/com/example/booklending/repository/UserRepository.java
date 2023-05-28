b96de2db01573164f1ba78d8158be8e87b84eb5e
Christine <71424154+christine40372@users.noreply.github.com>
2023-05-28 17:16:57 +0800

    Update UserRepository.java

--- src/main/java/com/example/booklending/repository/UserRepository.java
+++ src/main/java/com/example/booklending/repository/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.booklending.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 可以添加其他自定義的查詢方法
+    
 }
