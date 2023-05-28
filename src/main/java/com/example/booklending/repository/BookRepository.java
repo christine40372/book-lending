7e5431b6a5d7b8c973915268c7b3b41a0ad987d1
Christine <71424154+christine40372@users.noreply.github.com>
2023-05-28 17:16:48 +0800

    Update BookRepository.java

--- src/main/java/com/example/booklending/repository/BookRepository.java
+++ src/main/java/com/example/booklending/repository/BookRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.booklending.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // 可以添加其他自定義的查詢方法
+    
 }
